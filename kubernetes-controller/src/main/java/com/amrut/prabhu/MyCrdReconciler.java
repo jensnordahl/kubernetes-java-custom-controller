7d13edf46bd794468f5126956a961a8ab85a6354
Jens Nordahl <jens.nordahl@chainalysis.com>
2023-11-01 13:38:03 +0100

    Rewrite reconciliation logic

--- kubernetes-controller/src/main/java/com/amrut/prabhu/MyCrdReconciler.java
+++ kubernetes-controller/src/main/java/com/amrut/prabhu/MyCrdReconciler.java
@@ -19,6 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -43,54 +44,131 @@ public class MyCrdReconciler implements Reconciler {
         logger.info("==== reconcile: " + request);
         String key = request.getNamespace() + "/" + request.getName();
 
-        V1MyCrd resourceInstance = myCrdInformer
-                .getIndexer()
-                .getByKey(key);
-
-        if (resourceInstance != null) {
-
-            V1ConfigMap v1ConfigMap = createConfigMap(resourceInstance);
-            logger.info("Creating resource...");
-
-            V1MyCrd copy = deepCopy(resourceInstance);
-            copy.setStatus(new V1MyCrdStatus().configMapId("fake-status"));
-            KubernetesApiResponse<V1MyCrd> update = myCrdApi.update(copy);
-            if (! update.isSuccess()) {
-                logger.warn("Failed to update, re-queueing: " + update.getStatus());
-                return new Result(true);
-            }
-
-            try {
-                coreV1Api.createNamespacedConfigMap(request.getNamespace(),
-                        v1ConfigMap,
-                        "true",
-                        null,
-                        "",
-                        "");
-            } catch (ApiException e) {
-                logger.info("Creating resource failed");
-                if (e.getCode() == 409) {
-                    logger.info("Updating resource...");
-                    try {
-                        coreV1Api.replaceNamespacedConfigMap(v1ConfigMap.getMetadata().getName(),
-                                request.getNamespace(),
-                                v1ConfigMap,
-                                "true",
-                                null,
-                                "",
-                                "");
-                    } catch (ApiException ex) {
-                        logger.warn("Failed updating resource..." + ex.getResponseBody());
-                        throw new RuntimeException(ex);
-                    }
-                } else {
-                    logger.warn("Failed creating resource (wrong reason)" + e.getResponseBody());
-                    throw new RuntimeException(e);
-                }
-            }
-            return new Result(false);
+        V1MyCrd resource = myCrdInformer.getIndexer().getByKey(key);
+        if (resource == null) {
+            // Resource deleted since it was added to the work queue - forget it
+            return new Result(true);
         }
+
+        String expectedChildName = childNameForResource(resource);
+        Map<String, String> expectedChildData = childDataForResource(resource);
+        boolean actualChildFound = false;
+
+        List<V1ConfigMap> actualChildren = findChildren(resource);
+        for (V1ConfigMap child : actualChildren) {
+            if (expectedChildName.equals(child.getMetadata().getName())) {
+                actualChildFound = true;
+                if (!expectedChildData.equals(child.getData())) {
+                    updateChild(child, expectedChildData);
+                }
+
+                updateStatus(resource, child);
+            } else {
+                deleteChild(child);
+            }
+        }
+
+        if (!actualChildFound) {
+            createChild(resource, expectedChildName, expectedChildData);
+        }
+
         return new Result(false);
+    }
+
+    private List<V1ConfigMap> findChildren(V1MyCrd resource) {
+        return configMapInformer.getIndexer().list().stream()
+                .filter(child -> isOwnedBy(resource, child))
+                .toList();
+    }
+
+    private boolean isOwnedBy(V1MyCrd resource, V1ConfigMap child) {
+        List<V1OwnerReference> ownerReferences = child.getMetadata().getOwnerReferences();
+        if (ownerReferences == null) {
+            return false;
+        }
+        return ownerReferences.stream()
+                .anyMatch(v1OwnerReference
+                        -> v1OwnerReference.getUid().equals(resource.getMetadata().getUid()));
+    }
+
+    private void createChild(V1MyCrd resourceInstance, String childName, Map<String, String> childData) {
+        logger.info("Creating child configmap: " + childName);
+        V1ConfigMap child = new V1ConfigMap()
+                .metadata(new V1ObjectMeta()
+                        .name(childName)
+                        .addOwnerReferencesItem(new V1OwnerReference()
+                                .apiVersion(resourceInstance.getApiVersion())
+                                .kind(resourceInstance.getKind())
+                                .name(resourceInstance.getMetadata().getName())
+                                .uid(resourceInstance.getMetadata().getUid())))
+                .data(childData);
+
+        try {
+            coreV1Api.createNamespacedConfigMap(
+                    resourceInstance.getMetadata().getNamespace(),
+                    child,
+                    "true",
+                    null,
+                    "",
+                    "");
+        } catch (ApiException e) {
+            throw new RuntimeException("Failed to create configmap", e);
+        }
+    }
+
+    private void updateChild(V1ConfigMap child, Map<String, String> childData) {
+        logger.info("Updating child configmap: " + child.getMetadata().getName());
+        V1ConfigMap copy = deepCopy(child);
+        copy.setData(childData);
+
+        try {
+            coreV1Api.replaceNamespacedConfigMap(copy.getMetadata().getName(),
+                    copy.getMetadata().getNamespace(),
+                    copy,
+                    "true",
+                    null,
+                    "",
+                    "");
+        } catch (ApiException e) {
+            throw new RuntimeException("Failed to update configmap", e);
+        }
+    }
+
+    private void deleteChild(V1ConfigMap child) {
+        logger.info("Deleting child configmap: " + child.getMetadata().getName());
+        try {
+            coreV1Api.deleteNamespacedConfigMap(
+                    child.getMetadata().getName(),
+                    child.getMetadata().getNamespace(),
+                    "true",
+                    null,
+                    0,
+                    false,
+                    "",
+                    null);
+        } catch (ApiException e) {
+            throw new RuntimeException("Failed to delete configmap", e);
+        }
+    }
+
+    private void updateStatus(V1MyCrd resource, V1ConfigMap child) {
+        logger.info("Updating status: " + child.getMetadata().getName());
+        V1MyCrd copy = deepCopy(resource);
+        copy.setStatus(new V1MyCrdStatus().configMapId(child.getMetadata().getUid()));
+        KubernetesApiResponse<V1MyCrd> apiResponse = myCrdApi.update(copy);
+        if (!apiResponse.isSuccess()) {
+            throw new RuntimeException("Failed to update status: " + apiResponse.getStatus());
+        }
+    }
+
+    private Map<String, String> childDataForResource(V1MyCrd resource) {
+        return Map.of("my-own-property", resource
+                .getSpec()
+                .getMyOwnProperty());
+    }
+
+    private String childNameForResource(V1MyCrd resource) {
+        return resource.getMetadata().getName() + "-child";
     }
 
     private static <T> T deepCopy(T t) {
